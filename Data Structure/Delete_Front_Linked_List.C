8e7320f0b562370fabd618aeb07c7b87330c080e
elangoram1998 <30955305+elangoram1998@users.noreply.github.com>
2018-03-19 15:39:46 +0530

    Create Delete_Front_Linked_List.C

---
+++ Data Structure/Delete_Front_Linked_List.C
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+typedef struct node n;
+n *head,*temp;
+
+int main() 
+{
+    int a=5;
+    while(a!=0)
+    {
+        create();
+        a--;
+    }
+    delete(head);
+    display();
+}
+create()
+{
+    int b;
+    scanf("%d",&b);
+    n *newnode;
+    newnode=(n*)malloc(sizeof(n));
+    newnode->data=b;
+    newnode->next=NULL;
+    if(head==NULL)
+    {
+        head=newnode;
+        temp=newnode;
+    }
+    else
+    {
+        temp->next=newnode;
+        temp=newnode;
+    }
+}
+delete(n *temp1)
+{
+    temp1=head;
+    head=head->next;
+    temp1->next=NULL;
+    free(temp1);
+}
+display()
+{
+    while(head!=NULL)
+    {
+        printf("%d ",head->data);
+        head=head->next;
+    }
+}
+
